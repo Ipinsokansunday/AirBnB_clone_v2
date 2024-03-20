cfa6f84c00d2ebe24851d0bd92cde9ade6ad4d5b
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-03-20 08:54:55 -0700

    0x02. AirBnB clone - MySQL

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+"""Defines unit tests for the State class."""
+
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
-
 class test_state(test_basemodel):
-    """ """
+    """Test suite for the State class."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes test_state instance."""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """Test that the 'name' attribute of State instances is a string."""
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-""" """
+"""Defines unit tests for the User class."""
+
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
-
 class test_User(test_basemodel):
-    """ """
+    """Test suite for the User class."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes test_User instance."""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """Test that the 'first_name' attribute of User instances is a string."""
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """Test that the 'last_name' attribute of User instances is a string."""
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """Test that the 'email' attribute of User instances is a string."""
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """Test that the 'password' attribute of User instances is a string."""
         new = self.value()
         self.assertEqual(type(new.password), str)
