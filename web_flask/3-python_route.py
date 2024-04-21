577694b79b2bcadc036076012a45289599523531
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-04-21 01:16:21 -0700

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Simple Flask web app with multiple routes."""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_world():
+    """Returns a simple greeting."""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hello():
+    """Returns a message mentioning HBNB."""
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Returns 'C' followed by modified text."""
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """Returns 'Python' followed by modified text (default: 'is cool')."""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
