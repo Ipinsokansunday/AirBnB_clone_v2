dca51bba4d1b68cdf39fcb37f18922503424536e
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-04-21 01:36:48 -0700

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-"""Simple Flask web app with multiple routes."""
+""" Add third view func that redirects and has default val for variable """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
-    """Returns a simple greeting."""
+    """ Returns some text. """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hello():
-    """Returns a message mentioning HBNB."""
+    """ Return other text. """
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_text(text):
-    """Returns 'C' followed by modified text."""
-    return 'C {}'.format(text.replace('_', ' '))
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_text(text='is cool'):
-    """Returns 'Python' by modified text (default: 'is cool')."""
-    return 'Python {}'.format(text.replace('_', ' '))
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
